560695272a020087f7bb88aae3452ec4481b15a7
Derek Medrano <derekjmedrano@gmail.com>
2023-12-04 13:39:51 -0600

    comments added

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function writePassword() {
 	passwordText.value = password;
 }
 
+// Prompts the user to supply the generator with criteria
 function generatePassword() {
 	var lengthQuestion = prompt("What is your desired password length? Must be 8 - 128 characters.")
 
@@ -22,12 +23,12 @@ function generatePassword() {
 	var numberQuestion = confirm("Would you like to include numbers?");
 	var specialCharacterQuestion = confirm("Would you like to include special characters?");
 
-
+// This conditional statement prevents the user from continuing the function without supplying any usable characters
 	if (!lowercaseQuestion && !uppercaseQuestion && !numberQuestion && !specialCharacterQuestion) {
 		alert("Password requirement not met.")
 		return;
 	}
-	
+// Variables are strings, which are later split into an array. The user's given criteria is added the the allChars variable, which later becomes the final generated password.
 	var allChars = ''
 	var lowercase = "abcdefghijklmnopqrstuvwxyz"
 
@@ -53,14 +54,14 @@ function generatePassword() {
 	var finalString = allChars.split('')
 
 	var passwordArray=[];
-
+// The password array is assigned values at random, and stops when the desired password length is reached.
 	for (var i = 0; i < lengthQuestion; i++) {
 	passwordArray.push(random(finalString));
 	}
 	
 	return passwordArray.join('')
 }
-
+//This function selects a value from an array, at random.
 function random(arr) {
 
 	return arr[Math.floor(Math.random() * arr.length)];
